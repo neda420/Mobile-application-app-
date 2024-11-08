7e47ae4864c8c33673c7230217abefd34e9f7616
neda420 <nadim015582@gmail.com>
2024-11-08 23:48:53 +0600

    Done

---
+++ Calculator2/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,99 @@
+
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText resultEditText;
+    private String expression = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultEditText = findViewById(R.id.result);
+    }
+
+    public void onButtonClick(View view) {
+        Button button = (Button) view;
+        String buttonText = button.getText().toString();
+
+        expression += buttonText;
+        resultEditText.setText(expression);
+    }
+
+    public void onClearClick(View view) {
+        expression = "";
+        resultEditText.setText("");
+    }
+
+    public void onEqualsClick(View view) {
+        try {
+            double result = evaluateExpression(expression);
+            resultEditText.setText(String.valueOf(result));
+        } catch (Exception e) {
+            resultEditText.setText("Error");
+        }
+    }
+
+    private double evaluateExpression(String expression) {
+        Stack<Double> values = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                StringBuilder sb = new StringBuilder();
+                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
+                    sb.append(expression.charAt(i++));
+                }
+                values.push(Double.parseDouble(sb.toString()));
+                i--; // adjust position after reading number
+            } else if (ch == '(') {
+                operators.push(ch);
+            } else if (ch == ')') {
+                while (operators.peek() != '(') {
+                    values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.pop();
+            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while (!operators.isEmpty() && hasPrecedence(ch, operators.peek())) {
+                    values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.push(ch);
+            }
+        }
+
+        while (!operators.isEmpty()) {
+            values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+        }
+
+        return values.pop();
+    }
+
+    private boolean hasPrecedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')') return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) return false;
+        return true;
+    }
+
+    private double applyOperator(char op, double b, double a) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/':
+                if (b == 0) throw new UnsupportedOperationException("Cannot divide by zero");
+                return a / b;
+        }
+        return 0;
+    }
+}
